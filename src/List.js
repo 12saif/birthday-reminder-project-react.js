9bd8b8c51cb4174d0538c59b3a172a7035bc6074
md saif <mohdsaif9170613062@gmail.com>
2023-01-09 00:49:27 +0530

    initial commit

---
+++ src/List.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const List = ({people}) => {
+  return (
+    <>
+    {people.map((person) =>{
+        const {id,name,age,image} = person;
+        return <artical key={id} className="person">
+            <img src={image} alt={name} />
+            <div>
+                <h4>{name}</h4>
+                <p>{age} years</p>
+            </div>
+        </artical>
+    })}
+    </>
+  );
+};
+
+export default List;
---
+++ src/data.js
@@ -0,0 +1,38 @@
+export default [
+    {
+      id: 1,
+      name: 'Md.Saif',
+      age: 20,
+      image:
+        'saifimage000.jpg',
+    },
+    {
+      id: 2,
+      name: 'Mohammad Umar',
+      age: 32,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+    },
+    {
+      id: 3,
+      name: 'Mohammad Abu Talha',
+      age: 36,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+    },
+    {
+      id: 4,
+      name: 'Harish Zia',
+      age: 34,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+    },
+    {
+      id: 5,
+      name: 'Saqlain Raza',
+      age: 29,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    },
+  ];
+  
